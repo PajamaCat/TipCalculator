8f0c7dcad9c27c1ede4fe3c543c01a5666845744
Jiafang Jiang <jiafang.jiang@airbnb.com>
2017-03-16 17:11:27 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by jiafang_jiang on 3/16/17.
+//  Copyright © 2017 jiafang_jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by jiafang_jiang on 3/16/17.
+//  Copyright © 2017 jiafang_jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
